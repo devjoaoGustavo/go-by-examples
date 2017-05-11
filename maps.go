9b87393ed4677b92d993306bdc7c56e41bc0744a
João Gustavo de Paula <joao.paula@locaweb.com.br>
2017-05-11 15:27:13 -0300

    A minute of practice

--- maps.go
+++ maps.go
@@ -9,16 +9,19 @@ func main() {
 	fmt.Println("map1:", m)
 
 	v1, bla := m["a"]
-	fmt.Println("getwith second value:", v1, bla)
+	fmt.Println("get with second value:", v1, bla)
 
 	fmt.Println("len:", len(m))
 
 	delete(m, "b")
 	fmt.Println("map after deleting:", m)
 
-	val, prs := m["b"]
-	fmt.Println("prs:", prs, val)
+	z_val, prs := m["b"]
+	fmt.Println("prs:", prs, z_val)
 
-	n := map[string]int{"age": 15, "point": 25}
+	_, coisa := m["k2"]
+	fmt.Println("the key was present:", coisa)
+
+	n := map[string]int{"age": 15, "point": 25, "timestamp": 1493737290}
 	fmt.Println("map:", n)
 }
