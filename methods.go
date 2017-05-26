fb57e287a52f1d27bb946f70d9070a9b23ecd2f0
João Gustavo de Paula <joao.paula@locaweb.com.br>
2017-05-26 10:37:26 -0300

    A little more of golang

--- methods.go
+++ methods.go
@@ -6,12 +6,20 @@ type rect struct {
   width, height int
 }
 
-func (r *rect) area() int {
+func (r rect) area() int {
   return r.width * r.height
 }
 
 func (r rect) perim() int {
   return 2*r.width + 2*r.height
+}
+
+func (r *rect) SetWidth(newWidth int) {
+  r.width = newWidth
+}
+
+func (r *rect) SetHeight(newHeight int) {
+  r.height = newHeight
 }
 
 func main() {
@@ -23,4 +31,11 @@ func main() {
   rp := &r
   fmt.Println("area:", rp.area())
   fmt.Println("perim:", rp.perim())
+
+  fmt.Println("Struct:", r)
+  r.SetWidth(15)
+  r.SetHeight(10)
+  fmt.Println("Modified struct:", r)
+  fmt.Println("bigger area:", r.area())
+  fmt.Println("bigger perim:", r.perim())
 }
